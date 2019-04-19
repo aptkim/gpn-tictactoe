193a8d8da5350b7257128d7b73e76ee7fdf8f21f
Kim Apted <aptkim@users.noreply.github.com>
2019-04-19 15:31:04 +1000

    Intermediate Part 6

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,5 +1,21 @@
 # Kim Apted is testing the intermediate work book
 
+winning_combos = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]]
+
+def check_winner(board):
+    for combo in winning_combos:
+        symbol_0 = board[combo[0]]
+        symbol_1 = board[combo[1]]
+        symbol_2 = board[combo[2]]
+
+        if symbol_0 == symbol_1 == symbol_2:
+            return True
+
+    return False
+
 def print_board(board):
     print("-------------")
     print("| {} | {} | {} |".format(board[0], board[1], board[2]))
