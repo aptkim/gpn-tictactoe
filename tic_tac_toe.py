3af2b4a43f767cfa0d03314942b251440f5487ed
Kim Apted <aptkim@users.noreply.github.com>
2019-04-19 13:05:00 +1000

    Intermediate Part 1

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,1 +1,11 @@
-# Kim Apted is testing the advanced work book
+# Kim Apted is testing the intermediate work book
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+print("-------------")
+print("| {} | {} | {} |".format(board[0], board[1], board[2]))
+print("-------------")
+print("| {} | {} | {} |".format(board[3], board[4], board[5]))
+print("-------------")
+print("| {} | {} | {} |".format(board[6], board[7], board[8]))
+print("-------------")
