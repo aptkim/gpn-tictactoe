8368917924a211a2867c3c9282322b91bd1f4dc9
Kim Apted <aptkim@users.noreply.github.com>
2019-04-19 13:13:15 +1000

    Intermediate Part 3

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,6 +1,16 @@
 # Kim Apted is testing the intermediate work book
 
+def print_board(board):
+    print("-------------")
+    print("| {} | {} | {} |".format(board[0], board[1], board[2]))
+    print("-------------")
+    print("| {} | {} | {} |".format(board[3], board[4], board[5]))
+    print("-------------")
+    print("| {} | {} | {} |".format(board[6], board[7], board[8]))
+    print("-------------")
+
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+print_board(board)
 
 symbol = "O"
 
@@ -8,11 +18,4 @@ square = input("Which square do you want to choose? ")
 square_index = int(square)
 
 board[square_index] = symbol
-
-print("-------------")
-print("| {} | {} | {} |".format(board[0], board[1], board[2]))
-print("-------------")
-print("| {} | {} | {} |".format(board[3], board[4], board[5]))
-print("-------------")
-print("| {} | {} | {} |".format(board[6], board[7], board[8]))
-print("-------------")
+print_board(board)
